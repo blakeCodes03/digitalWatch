2067002e115119a241ac59bcbd3721edd29a27e6
blakeCodes03 <jahotanenlap@gmail.com>
2022-10-24 00:29:41 +0100

    first commit

---
+++ digitalWatch.js
@@ -0,0 +1,29 @@
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    h = checkTime(h)
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      startTime()
+    }, 500);
+  }
+  startTime();
+
+//   button clicking to activate watch
+var showTime = document.getElementById('showTime')
+showTime.onclick = function(){
+    document.getElementById('time').classList.add('animated', 'fadeInRight');
+    document.getElementById('time').classList.toggle('digitalClock');
+}
